cf63933d452527ce6dd8ba38236bca60f43998f3
Arin Agarwal <agararin@amazon.com>
2026-05-09 19:24:07 -0700

    bug fixes modified cocomo

--- modified_cocomo/pipeline.py
+++ modified_cocomo/pipeline.py
@@ -16,8 +16,10 @@ from __future__ import annotations
 
 import sys
 import os
-sys.path.insert(0, os.path.dirname(__file__))
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'cocomo'))
+
+_HERE = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, _HERE)
+sys.path.insert(1, os.path.join(_HERE, '..', 'cocomo'))
 
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
